56f95f991e57f474ac14542bc5d400deab248d38
Dianzheng <dianzheng@DianzhengdeMacBook-Air.local>
2026-05-29 15:36:41 +0800

    Revert navbar and sidebar changes.

--- docs/.vitepress/theme/sidebar/en.ts
+++ docs/.vitepress/theme/sidebar/en.ts
@@ -105,7 +105,7 @@ export const enSidebar: DefaultTheme.Sidebar = {
       text: "Purchase & Activation",
       collapsed: false,
       items: [
-        // { text: "Online Payment & Activation", link: "/license/online-payment-activation" }, // [Hidden] content not yet online
+        { text: "Online Payment & Activation", link: "/license/online-payment-activation" },
         {
           text: "Activation Code & Activation",
           link: "/license/activation-code-activation"
